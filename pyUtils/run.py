aa67b0cbdb9d6107490477ea116a825327786e03
Soku <36831178+YcSoku@users.noreply.github.com>
2025-02-05 02:05:29 +0800

    Update sbms (#48)
    * Update sbms
    
    * Merge branch 'main' into dev_feature

--- pyUtils/run.py
+++ pyUtils/run.py
@@ -1,13 +1,5 @@
-# from backend import create_app, APP_PORT, APP_DEBUG
-    
-# if __name__ == '__main__':
-
-#     app = create_app()
-    
-#     app.run(host = "0.0.0.0", port = APP_PORT, debug = APP_DEBUG)
-
 import sbms
 
 if __name__ == '__main__':
-    
+    sbms.registry.update_registry({'/v0/fe/hello': 'example/hello.py'})
     sbms.run()
