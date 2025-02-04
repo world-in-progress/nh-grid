5fc200fa68ce38becd4723100c0f74cbd1849dc9
Soku <36831178+YcSoku@users.noreply.github.com>
2025-02-05 01:00:12 +0800

    Dev_feature (#47)
    * Update flask backend
    
    * Update flask backend

--- pyUtils/run.py
+++ pyUtils/run.py
@@ -1,7 +1,13 @@
-from backend import create_app, APP_PORT, APP_DEBUG
+# from backend import create_app, APP_PORT, APP_DEBUG
     
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    app = create_app()
+#     app = create_app()
     
-    app.run(host = "0.0.0.0", port = APP_PORT, debug = APP_DEBUG)
+#     app.run(host = "0.0.0.0", port = APP_PORT, debug = APP_DEBUG)
+
+import sbms
+
+if __name__ == '__main__':
+    
+    sbms.run()
