832eceeb72d48e5e74b91a67a0152cf7409d455e
UyNauh Loo <133184327+unaLoo@users.noreply.github.com>
2025-02-18 12:39:09 +0800

    NHLayerGroup - init (#61)
    * update
    
    * processing-grid
    
    * Merge branch 'Liquordynamic:main' into main
    
    * Merge branch 'Liquordynamic:main' into main
    
    * Merge branch 'Liquordynamic:main' into main
    
    * Merge branch 'Liquordynamic:main' into main
    
    * Merge branch 'Liquordynamic:main' into main
    
    * Merge branch 'Liquordynamic:main' into main
    
    * Merge branch 'Liquordynamic:main' into main
    
    * update main
    
    * Merge branch 'WorldInProgress:main' into main
    
    * update
    
    * Merge branch 'WorldInProgress:main' into main
    
    * update-main
    
    * update
    
    * Merge branch 'dev' of https://github.com/unaLoo/NHGrid into dev

---
+++ demo/map2/types-and-interfaces/type.ts
@@ -0,0 +1,11 @@
+import { Map } from "mapbox-gl";
+
+type NHCustomeLayerOnAddMethod = (map: Map, gl: WebGL2RenderingContext) => void;
+
+type NHCustomeLayerRenderMethod = (gl: WebGL2RenderingContext, matrix: Array<number>) => void
+
+
+export {
+    type NHCustomeLayerRenderMethod,
+    type NHCustomeLayerOnAddMethod
+}
